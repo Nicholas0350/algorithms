271f235e07391f31f70c3213f17e864e093e477d
nicholas <nicholas@zainaweb.com>
2021-11-12 11:38:54 +1100

    find closest value algo

---
+++ binarySearchTrees/findClosestValueInBst.js
@@ -0,0 +1,22 @@
+// function that takes a binary search tree and a target value and returns the closest value to the target.
+
+
+//time complexity: O(log(n))
+//space complexity: O(1)
+
+function findClosestValueInBst(tree, target) {
+
+  let currentNode = tree;
+  let closest = null;
+  while (currentNode) {
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value;
+    }
+    if (target < currentNode.value) {
+      currentNode = currentNode.left;
+    } else {
+      currentNode = currentNode.right;
+    }
+  }
+  return closest;
+}
